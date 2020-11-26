ca1a752aee1497f01e642c614dc3d1450b30df4d
HarrisonHenri <harrisonhenrisn@onyo.com>
2020-11-25 21:02:51 -0300

    finish project

--- Readme.md
+++ Readme.md
@@ -1,1 +1,74 @@
-## In development...
+## Why?
+
+This project is part of my personal portfolio, so, I'll be happy if you could provide me any feedback about the project, code, structure or anything that you can report that could make me a better developer!
+
+Email-me: harrisonhenrisn@gmail.com
+
+Connect with me at [LinkedIn](https://linkedin.com/in/harrison-henri-dos-santos-nascimento-a6ba33112).
+
+Also, you can use this Project as you wish, be for study, be for make improvements or earn money with it!
+
+It's free!
+
+## Install
+
+Clone the repo using
+
+```
+$ git clone https://github.com/HarrisonHenri/flask-api
+```
+
+```
+$ cd flask-api
+```
+
+Install all the required dependencies (all directories)
+
+```
+$ pip install -r requirements.txt
+```
+
+## Built With
+
+- alembic
+- aniso8601
+- attrs
+- click
+- flasgger
+- Flask
+- Flask-JWT-Extended
+- flask-marshmallow
+- Flask-Migrate
+- Flask-RESTful
+- Flask-SQLAlchemy
+- importlib-metadata
+- itsdangerous
+- Jinja22
+- jsonschema
+- Mako
+- MarkupSafe
+- marshmallow
+- marshmallow-sqlalchemy
+- mistune
+- passlib
+- psycopg2
+- PyJWT
+- pyrsistent
+- python-dateutil
+- python-editor
+- pytz
+- PyYAML
+- six0
+- SQLAlchemy0
+- Werkzeug
+- zipp
+
+## Contributing
+
+You can send how many PR's do you want, I'll be glad to analyse and accept them! 
+
+Email-me: harrisonhenrisn@gmail.com
+
+Connect with me at [LinkedIn](https://linkedin.com/in/harrison-henri-dos-santos-nascimento-a6ba33112).
+
+Thank you!
---
+++ api/views/funcionario_views.py
@@ -0,0 +1,70 @@
+from flask_restful import Resource
+from api import api
+from ..schemas import funcionario_schema
+from flask import request, make_response, jsonify
+from ..entidades import funcionario
+from ..services import funcionario_service
+from ..models.funcionario_model import Funcionario
+from ..pagination import paginate
+
+class FuncionarioList(Resource):
+    def get(self):
+        # funcionarios = funcionario_service.listar_funcionarios()
+        fs = funcionario_schema.FuncionarioSchema(many=True)
+        return paginate(Funcionario, fs)
+
+    def post(self):
+        fs = funcionario_schema.FuncionarioSchema()
+        validate = fs.validate(request.json)
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            nome = request.json["nome"]
+            idade = request.json["idade"]
+
+            funcionario_novo = funcionario.Funcionario(nome=nome, idade=idade)
+            result = funcionario_service.cadastrar_funcionario(funcionario_novo)
+            return make_response(fs.jsonify(result), 201)
+
+
+class FuncionarioDetail(Resource):
+    def get(self, id):
+        funcionario = funcionario_service.listar_funcionarios_id(id)
+        if funcionario is None:
+            return make_response(jsonify("Funcionario não encontrado"), 404)
+
+        fs = funcionario_schema.FuncionarioSchema()
+        return make_response(fs.jsonify(funcionario), 200)
+
+    def put(self, id):
+        funcionario_bd = funcionario_service.listar_funcionarios_id(id)
+        if funcionario_bd is None:
+            return make_response(jsonify("Funcionario não encontrado"), 404)
+
+        fs = funcionario_schema.FuncionarioSchema()
+        validate = fs.validate(request.json)
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            nome = request.json["nome"]
+            idade = request.json["idade"]
+
+            funcionario_novo = funcionario.Funcionario(nome=nome, idade=idade)
+
+            funcionario_service.editar_funcionario(funcionario_bd, funcionario_novo)
+
+            funcionario_atualizada = funcionario_service.listar_funcionarios_id(id)
+
+            return make_response(fs.jsonify(funcionario_atualizada), 200)
+
+    def delete(self, id):
+        funcionario_bd = funcionario_service.listar_funcionarios_id(id)
+        if funcionario_bd is None:
+            return make_response(jsonify("Funcionario não encontrado"), 404)
+
+        funcionario_service.remover_funcionario(funcionario_bd)
+        return make_response('', 204)
+
+
+api.add_resource(FuncionarioList, '/funcionarios')
+api.add_resource(FuncionarioDetail, '/funcionario/<int:id>')
